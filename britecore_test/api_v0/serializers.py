94d180538150eea845577449a5281019e2ef46d3
Dmitry Danilov <danilovdmitry94@gmail.com>
2019-03-11 02:01:32 +0200

    Provide basic serializers

---
+++ britecore_test/api_v0/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+
+from risks.models import RiskType, Risk
+
+
+class JSONSerializerField(serializers.Field):
+    def to_internal_value(self, data):
+        return data
+
+    def to_representation(self, value):
+        return value
+
+
+class RiskTypeSerializer(serializers.ModelSerializer):
+
+    schema = JSONSerializerField()
+
+    class Meta:
+        model = RiskType
+        fields = (
+            'type_name',
+            'schema',
+            'is_active',
+        )
+
+
+class RiskSerializer(serializers.ModelSerializer):
+
+    risk_type = RiskTypeSerializer(read_only=True, required=True)
+    risk_data = JSONSerializerField()
+
+    class Meta:
+        model = Risk
+        fields = (
+            'risk_type',
+            'risk_data',
+        )
