00ba8f4bc707b9774bd0e1d16cb8e1e9b5d08d24
Dmitry Danilov <danilovdmitry94@gmail.com>
2019-03-13 00:40:08 +0200

    Make template for app and provide appropriate url

--- britecore_test/britecore_test/urls.py
+++ britecore_test/britecore_test/urls.py
@@ -17,8 +17,10 @@ from django.conf.urls import url, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^api/v0/', include('api_v0.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', TemplateView.as_view(template_name="index.html"), name='app'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
