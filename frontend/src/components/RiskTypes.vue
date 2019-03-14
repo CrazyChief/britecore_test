e485bcabb6690f49ae4e7bb23dfbe4b867b9cb44
Dmitry Danilov <danilovdmitry94@gmail.com>
2019-03-14 08:23:44 +0200

    Provide dashboard with separate route and RiskType component

---
+++ frontend/src/components/RiskTypes.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-flex xs12>
+        <v-card dark color="primary">
+            <v-toolbar flat color="green">
+              <v-toolbar-title>Risk Types</v-toolbar-title>
+                <p v-show="riskTypes"></p>
+              <v-divider
+                class="mx-2"
+                inset
+                vertical
+              ></v-divider>
+              <v-spacer></v-spacer>
+                <v-btn fab dark small color="primary">
+                  <v-icon dark>add</v-icon>
+                </v-btn>
+            </v-toolbar>
+            <v-list>
+              <v-list-tile
+                v-for="item in riskTypeItems"
+                :key="item.type_name"
+                @click=""
+              >
+                <v-list-tile-content>
+                  <v-list-tile-title v-text="item.type_name"></v-list-tile-title>
+                </v-list-tile-content>
+
+              </v-list-tile>
+            </v-list>
+        </v-card>
+    </v-flex>
+</template>
+
+<script>
+    export default {
+        name: 'RiskTypes',
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-content>
+        <v-container grid-list-xl>
+            <v-layout align-space-between justify-space-between row fill-height>
+                <RiskTypes />
+            </v-layout>
+        </v-container>
+    </v-content>
+</template>
+
+<script>
+import RiskTypes from "../components/RiskTypes";
+
+
+export default {
+    name: "Dashboard",
+    components: {
+        RiskTypes,
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
