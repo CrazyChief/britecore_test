deeae566ce8779fcb96e6bf359fa24f92df9b552
Dmitry Danilov <danilovdmitry94@gmail.com>
2019-03-14 08:20:31 +0200

    Added vuetify; Update views with start page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <v-app>
+    <v-toolbar app color="green">
+      <v-toolbar-title class="headline text-uppercase">
+        <span>BriteCore </span>
+        <span class="font-weight-light">Risk Types and Risks</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+
     <router-view />
-  </div>
+
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+  export default {
+    name: 'App'
   }
-}
-</style>
+</script>
