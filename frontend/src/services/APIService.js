97c7cbb382548395a971864b6b4321ffa9935f2a
Dmitry Danilov <danilovdmitry94@gmail.com>
2019-03-17 05:44:28 +0200

    Align code

--- frontend/src/services/APIService.js
+++ frontend/src/services/APIService.js
@@ -5,49 +5,49 @@ import { API_URL } from "./config";
 
 export class APIService {
 
-    constructor() {
-        Vue.use(VueAxios, axios);
-        Vue.axios.defaults.baseURL = API_URL;
-    }
+  constructor() {
+    Vue.use(VueAxios, axios);
+    Vue.axios.defaults.baseURL = API_URL;
+  }
 
-    getAllRiskTypes() {
-        const url = `${API_URL}risk-types/`;
-        return Vue.axios.get(url)
-            .then((response) => {
-                return response;
-            })
-            .catch((error) => {
-                throw new Error(`ApiService ${error}`);
-            });
-    }
+  getAllRiskTypes() {
+    const url = `${API_URL}risk-types/`;
+    return Vue.axios.get(url)
+      .then((response) => {
+        return response;
+      })
+      .catch((error) => {
+        throw new Error(`ApiService ${error}`);
+      });
+  }
 
-    getSingleRiskType(id) {
-        const url = `${API_URL}risk-types/${id}/`;
-        return Vue.axios.get(url)
-            .then((response) => {
-                return response;
-            })
-            .catch((error) => {
-                throw new Error(`ApiService ${error}`);
-            });
-    }
+  getSingleRiskType(id) {
+    const url = `${API_URL}risk-types/${id}/`;
+    return Vue.axios.get(url)
+      .then((response) => {
+        return response;
+      })
+      .catch((error) => {
+        throw new Error(`ApiService ${error}`);
+      });
+  }
 
-    createRiskType(data) {
-        const url = `${API_URL}risk-types/`;
-        return Vue.axios.post(url, data)
-            .then((response) => {
-                return response;
-            })
-            .catch((error) => {
-                throw new Error(`ApiService ${error}`);
-            });
-    }
+  createRiskType(data) {
+    const url = `${API_URL}risk-types/`;
+    return Vue.axios.post(url, data)
+      .then((response) => {
+        return response;
+      })
+      .catch((error) => {
+        throw new Error(`ApiService ${error}`);
+      });
+  }
 
-    updateRiskType(id, data) {
-        const url = `${API_URL}risk-types/${id}/`;
-        return Vue.axios.put(url, data)
-            .then((response) => {
-                return response;
-            });
-    }
+  updateRiskType(id, data) {
+    const url = `${API_URL}risk-types/${id}/`;
+    return Vue.axios.put(url, data)
+      .then((response) => {
+        return response;
+      });
+  }
 }
