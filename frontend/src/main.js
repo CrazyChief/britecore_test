b3f01a79f5b9d50f748af1c41c77a38a7c7350c2
Dmitry Danilov <danilovdmitry94@gmail.com>
2019-03-17 05:41:12 +0200

    Provide vee-validate to app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import './plugins/vuetify'
 import App from "./App.vue";
+import VeeValidate from 'vee-validate'
 import router from "./router";
 import store from "./store";
+
+Vue.use(VeeValidate);
 
 Vue.config.productionTip = false;
 
